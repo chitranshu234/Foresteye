0876638ccdd979b7040d15e8d895258bd2d207c9
Chitranshu Pandey <chitranshupandey234@gmail.com>
2026-05-24 15:10:57 +0530

    changes done -v1

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANqn5SNvO9ZGuUzuT15aDqh3tc6qH8Mn4",
-  authDomain: "iot-project-29231.firebaseapp.com",
-  databaseURL: "https://iot-project-29231-default-rtdb.firebaseio.com",
-  projectId: "iot-project-29231",
-  storageBucket: "iot-project-29231.firebasestorage.app",
-  messagingSenderId: "104185596211",
-  appId: "1:104185596211:web:7618fdbe924d96d7fe9646",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
